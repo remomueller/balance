86bb65d1281937283e5c0b3f6c314ea9b517a868
Remo Mueller <remosm@gmail.com>
2011-06-19 19:43:56 -0400

    Dashboard now loads again properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,6 @@ class UsersController < ApplicationController
     if params[:month] and params[:year]
       @start_date = Date.parse("#{params[:year]}-#{params[:month]}-01")
       @end_date = Date.parse("#{params[:year].to_i+params[:month].to_i/12}-#{(params[:month].to_i)%12+1}-01")-1.day
-      render :update do |page|
-        page.replace_html 'spending', :partial => 'accounts/month_spending'
-        page.replace_html 'month-breakdown', :partial => 'users/month_breakdown'
-      end
     end
   end
 end
