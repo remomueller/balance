d1790346cabdb40e25c8566319f2e40dd8128521
Remo Mueller <remosm@gmail.com>
2011-10-16 16:25:46 -0400

    Version 1.3.0 finalized

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -3,7 +3,7 @@ module Balance
     MAJOR = 1
     MINOR = 3
     TINY = 0
-    BUILD = 'pre' # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
