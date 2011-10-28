8f4abb7e6fef68b84b049d1fb5d0ba1785c14c0e
Remo Mueller <remosm@gmail.com>
2011-10-27 20:30:05 -0400

    Removing unused code for google charts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,31 +2,11 @@ module ApplicationHelper
   def cancel
     link_to image_tag('contour/cross.png', :alt => '') + 'Cancel', URI.parse(request.referer.to_s).path.blank? ? root_path : (URI.parse(request.referer.to_s).path), :class => 'button negative'
   end
-
-  def validation_errors(obj)
-    if obj and obj.errors.count > 0
-      update_page_tag do |page|
-        obj.errors.each do |attr,msg|
-          page.replace_html "#{attr}_status", msg
-        end
-      end
-    end
-  end
   
   def draw_chart(chart_api, chart_type, values, chart_element_id, chart_params, categories)
-    if chart_api == 'google'
-      google_chart(chart_type, values, chart_element_id, chart_params)
-    elsif chart_api == 'highcharts'
+    if chart_api == 'highcharts'
       highcharts_chart(chart_type, values, chart_element_id, chart_params, categories)
     end
-  end
-  
-  def google_chart(chart_type, values, chart_element_id, chart_params)
-    @values = values
-    @chart_element_id = chart_element_id
-    @chart_type = chart_type
-    @chart_params = chart_params
-    render :partial => 'google/google_chart'
   end
   
   def highcharts_chart(chart_type, values, chart_element_id, chart_params, categories)
