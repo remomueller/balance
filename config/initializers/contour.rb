0a8637a438acbb9f8b117b82d6a605b431c8d627
Remo Mueller <remosm@gmail.com>
2011-12-13 19:19:28 -0500

    Using Contour 0.7.0

--- config/initializers/contour.rb
+++ config/initializers/contour.rb
@@ -18,37 +18,33 @@ Contour.setup do |config|
   
   # Enter the items you wish to see in the menu
   config.menu_items = [{
-    :name => 'Login', :id => 'auth', :display => 'not_signed_in', :position => 'right', :position_class => 'right', :condition => 'true',
-    :links => [{:name => 'Login', :path => 'new_user_session_path'}, {:html => "<hr>"}, {:name => 'Sign Up', :path => 'new_user_registration_path'}]
+    :name => 'Login', :id => 'auth', :display => 'not_signed_in', :path => 'new_user_session_path', :position => 'right', :condition => 'true',
+    :links => [{:name => 'Sign Up', :path => 'new_user_registration_path'}]
   },
   {
-    :name => 'current_user.name', :eval => true, :id => 'auth', :display => 'signed_in', :position => 'right', :position_class => 'right', :condition => 'true',
-    :links => [{:html => '"<div style=\"white-space:nowrap\">"+(current_user.methods.include?(:name) ? current_user.name.to_s : "")+"</div>"', :eval => true}, {:html => '"<div class=\"small quiet\">"+current_user.email+"</div>"', :eval => true}, {:name => 'Authentications', :path => 'authentications_path', :condition => 'not PROVIDERS.blank?'}, {:html => "<hr>"}, {:name => 'Logout', :path => 'destroy_user_session_path'}]
+    :name => 'current_user.name', :eval => true, :id => 'auth', :display => 'signed_in', :path => 'root_path', :position => 'right', :condition => 'true',
+    :links => [{:html => '"<div class=\"small quiet\">"+current_user.email+"</div>"', :eval => true}, {:name => 'Authentications', :path => 'authentications_path', :condition => 'not PROVIDERS.blank?'}, {:html => "<br />"}, {:name => 'Logout', :path => 'destroy_user_session_path'}]
   },
   {
-    :name => 'Home', :id => 'home', :display => 'always', :position => 'left', :position_class => 'left',
-    :links => [{:name => 'Home', :path => 'root_path'},
-               {:name => 'Current Balance', :path => 'current_balance_entries_path'},
+    :name => 'Home', :id => 'home', :display => 'always', :path => 'root_path', :position => 'left',
+    :links => [{:name => 'Current Balance', :path => 'current_balance_entries_path'},
                {:name => 'Averages', :path => 'averages_entries_path'}]
   },
   {
-    :name => 'Graphs', :id => 'graphs', :display => 'signed_in', :position => 'left', :position_class => 'left_center',
-    :links => [{:name => 'Graphs', :path => 'overview_entries_path'}]
+    :name => 'Graphs', :id => 'graphs', :display => 'signed_in', :path => 'overview_entries_path', :position => 'left',
+    :links => []
   },
   {
-    :name => 'Accounts', :id => 'accounts', :display => 'signed_in', :position => 'left', :position_class => 'left_center',
-    :links => [{:name => 'Accounts', :path => 'accounts_path'},
-               {:name => '&raquo;New', :path => 'new_account_path'}]
+    :name => 'Accounts', :id => 'accounts', :display => 'signed_in', :path => 'accounts_path', :position => 'left',
+    :links => [{:name => '&raquo;New', :path => 'new_account_path'}]
   },
   {
-    :name => 'Charge Types', :id => 'charge_types', :display => 'signed_in', :position => 'left', :position_class => 'left_center',
-    :links => [{:name => 'Charge Types', :path => 'charge_types_path'},
-               {:name => '&raquo;New', :path => 'new_charge_type_path'}]
+    :name => 'Charge Types', :id => 'charge_types', :display => 'signed_in', :path => 'charge_types_path', :position => 'left',
+    :links => [{:name => '&raquo;New', :path => 'new_charge_type_path'}]
   },
   {
-    :name => 'Entries', :id => 'entries', :display => 'signed_in', :position => 'left', :position_class => 'left_center',
-    :links => [{:name => 'Entries', :path => 'entries_path'},
-               {:name => '&raquo;New', :path => 'new_entry_path'}]
+    :name => 'Entries', :id => 'entries', :display => 'signed_in', :path => 'entries_path', :position => 'left',
+    :links => [{:name => '&raquo;New', :path => 'new_entry_path'}]
   }
   ]
   
