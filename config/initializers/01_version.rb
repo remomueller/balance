0d163fdff39a2a7fac1da28caf95802147509bfb
Remo Mueller <remosm@gmail.com>
2016-03-13 18:12:03 -0400

    Version bump to 2.0.0.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Balance
   module VERSION #:nodoc:
-    MAJOR = 1
-    MINOR = 11
+    MAJOR = 2
+    MINOR = 0
     TINY = 0
     BUILD = 'pre' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
