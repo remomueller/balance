4206029bda2e3130fd0272cd76946910fe1404cc
Remo Mueller <remosm@gmail.com>
2016-08-14 22:11:03 -0400

    Simplified styling of current balance page

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,40 +1,40 @@
-class Account < ActiveRecord::Base
+# frozen_string_literal: true
 
+# Stores the total value of a single account. Can have multiple charge types
+# that can count towards spending or be transfers.
+class Account < ActiveRecord::Base
   # Concerns
   include Searchable, Deletable
 
-  # Named Scopes
-
   # Model Validation
-  validates_presence_of :name
+  validates :name, presence: true
 
   # Model Relationships
   belongs_to :user
-  has_many :charge_types, -> { where(deleted: false).order(:name) }
+  has_many :charge_types, -> { current.order(:name) }
 
   # Account Methods
 
   def total_spent
     @total_spent ||= begin
-      self.charge_types.collect(&:total_spent).sum
+      charge_types.collect(&:total_spent).sum
     end
   end
 
   # type: :amount, :charged_amount
   def balance(type)
-    self.charge_types.collect{|c| c.contribution(type)}.sum
+    charge_types.collect { |c| c.contribution(type) }.sum
   end
 
   def spent_in_time_period(start_date, end_date)
     @spent_in_time_period ||= begin
-      self.charge_types.collect{|item| item.spent_in_time_period(start_date,end_date)}.sum
+      charge_types.collect { |item| item.spent_in_time_period(start_date, end_date) }.sum
     end
   end
 
   def average_spent_over_time_period(start_date, end_date)
     @average_spent_over_time_period ||= begin
-      self.spent_in_time_period(start_date, end_date) / (end_date - start_date + 1.0)
+      spent_in_time_period(start_date, end_date) / (end_date - start_date + 1.0)
     end
   end
-
 end
