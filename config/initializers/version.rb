f8cdf2089f64b00c52b55e85b91a83ed053a1f29
Remo Mueller <remosm@gmail.com>
2017-05-16 19:59:12 -0400

    Version bump to 2.4.0.rc

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,8 +5,8 @@ module Balance
     MAJOR = 2
     MINOR = 4
     TINY = 0
-    BUILD = 'pre' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = "rc" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".")
   end
 end
