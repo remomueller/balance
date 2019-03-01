3c4d828e2ae5b9f45b4488d236a8d155acdff50a
Remo Mueller <remosm@gmail.com>
2019-02-28 19:39:00 -0500

    Update to rails 6.0.0.beta1

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-ActionMailer::Base.default_url_options[:host] = ENV['website_url'].to_s.gsub(%r{^https?://}, '')
+ActionMailer::Base.default_url_options[:host] = ENV["website_url"].to_s.gsub(%r{^https?://}, "")
 if Rails.env.test?
   ActionMailer::Base.delivery_method = :test
-  ActionMailer::Base.smtp_settings[:email] = 'travis-ci@example.com'
+  ActionMailer::Base.smtp_settings[:email] = "travis-ci@example.com"
   EMAILS_ENABLED = true
 else
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: ENV['smtp_address'],
-    port: ENV['smtp_port'].to_i,
-    authentication: ENV['smtp_authentication'].to_sym, # :plain, :login, or, :cram_md5
-    email: ENV['smtp_email'],
-    user_name: ENV['smtp_user_name'],
-    password: ENV['smtp_password']
+    address: Rails.application.credentials.dig(:smtp_address),
+    port: Rails.application.credentials.dig(:smtp_port).to_i,
+    authentication: Rails.application.credentials.dig(:smtp_authentication).to_sym, # :plain, :login, or, :cram_md5
+    email: Rails.application.credentials.dig(:smtp_email),
+    user_name: Rails.application.credentials.dig(:smtp_user_name),
+    password: Rails.application.credentials.dig(:smtp_password)
   }
-  EMAILS_ENABLED = (ENV['emails_enabled'] == 'true')
+  EMAILS_ENABLED = (ENV["emails_enabled"] == "true")
 end
